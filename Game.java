ee62f22228cba12f8b6e7749ec804daa89e8fb5b
Zachary <42364001+MagnusChase03@users.noreply.github.com>
2018-10-01 11:53:58 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,1 +1,30 @@
 
+import javax.swing.*;
+import java.awt.*;
+import java.lang.Thread;
+
+public class Game extends JPanel implements Runnable {
+  
+  public Game() {
+    
+  }
+  
+  @Override
+  public void paint(Graphics g) {
+  
+    g.setColor(new Color(30, 160, 30));
+    g.fillRect(0, 0, 768, 768);
+  
+  }
+
+  @Override
+  public void run() {
+  
+    try {
+      Thread.sleep(1000/30);
+    } catch (Exception e) { 
+    }
+    
+  }
+  
+}
