0ea7b5a1c550f2a942266653bc4a6473663b464d
Zachary <42364001+MagnusChase03@users.noreply.github.com>
2018-10-01 11:45:30 -0500

    Update SurvivalGame.java

--- SurvivalGame.java
+++ SurvivalGame.java
@@ -1,1 +1,20 @@
+import javax.swing.*;
 
+public class SurvivalGame extends JFrame {
+
+  public SurvivalGame() {
+  
+    setTitle("Survival Game");
+    setSize(768, 768);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+  
+  }
+  
+  public static void main(String[] args) {
+
+    new SurvivalGame();
+  
+  }
+
+}
