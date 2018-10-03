9452048e6318afa3b7099154bbda05492b6b7fc0
Zachary <42364001+MagnusChase03@users.noreply.github.com>
2018-10-03 11:58:33 -0500

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,9 +1,23 @@
-public class Enemy{
+import java.awt.*;
 
-  public static void main(String[] args){
+public class Enemy {
+
+  public int x;
+  public int y;
+
+  public Enemy() {
    
+    x = (int) (Math.random() * 22);
+    y = (int) (Math.random() * 22);
     
     
+  }
+  
+  public void draw(Graphics g) {
+  	
+  	g.setColor(new Color(190, 0, 0));
+  	g.fillRect(x * 32 + 1, y * 32 + 1, 31, 31);
+  	
   }
   
 }
