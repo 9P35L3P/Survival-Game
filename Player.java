16a0acc01db854efb539c95fd6a7a8bc70a70ff3
Zachary <42364001+MagnusChase03@users.noreply.github.com>
2018-10-03 11:58:19 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -5,9 +5,8 @@ public class Player{
   public int x = 11;
   public int y = 11;
 
-  public Player(){
+  public Player() {
   
-    
     
   }
 
@@ -16,6 +15,30 @@ public class Player{
     g.setColor(new Color(0, 160, 190));
     g.fillRect(x * 32 + 1, y * 32 + 1, 31, 31);
 
+  }
+  
+  public void moveRight() {
+  	
+  	x++;
+  	
+  }
+  
+  public void moveLeft() {
+  	
+  	x--;
+  	
+  }
+  
+  public void moveUp() {
+  	
+  	y--;
+  	
+  }
+  
+  public void moveDown() {
+  	
+  	y++;
+  	
   }
   
 }
