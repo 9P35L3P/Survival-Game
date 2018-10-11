446c520edbb022667063d8c67cae8d9b3555d90b
Zachary <42364001+MagnusChase03@users.noreply.github.com>
2018-10-11 11:27:02 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -7,9 +7,57 @@ public class Player {
 
   public Player() {
 
-    x = (int) (Math.random() * 24);
-    y = (int) (Math.random() * 20);
+    x = (int) (Math.random() * 22 + 1);
+    y = (int) (Math.random() * 18 + 1);
 
+  }
+  
+  public void move(String dir) {
+  	
+  	switch (dir) {
+  		
+	  case "W":
+	    if (Game.board[y - 1][x] == 0) {
+  	
+		    Game.board[y][x] = 0;
+		    y--;
+		    Game.moveEnemy();
+		
+		  }
+		  break;
+		
+	  case "A":
+	    if (Game.board[y][x - 1] == 0) {
+      		
+		    Game.board[y][x] = 0;
+		    x--;
+		    Game.moveEnemy();
+      	
+		  }
+		  break;
+	
+	  case "S":
+	  	if (Game.board[y + 1][x] == 0) {
+		      
+		    Game.board[y][x] = 0;
+		    y++;
+		    Game.moveEnemy();
+      	
+		  }
+		  break;
+	
+	  case "D":
+	  	if (Game.board[y][x + 1] == 0) {
+      		
+		    Game.board[y][x] = 0;
+	  	  x++;
+		    Game.moveEnemy();
+      	
+		  }
+		  break;
+  		
+  	}
+  	
   }
 
 }
